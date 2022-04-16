67d0255de9888570a9f52920d054d69189e73378
tankbottoms <m@tankbottoms.xyz>
2022-04-16 02:24:55 -0400

    updated token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -20,14 +20,14 @@ contract Token is IToken, ERC721, Ownable {
         return _totalSupply;
     }
 
-    function tokenUri(uint256 tokenId) public view override returns (string memory) {
+     function tokenUri(uint256 tokenId) public view override returns (string memory) {
         string memory html = '<script>let playing=false</script><img src="https://cloudflare-ipfs.com/ipfs/QmdmPHWQBzV24GvbwCszm2AnWetBENeBP2UStuETsyAp1C" width="400" /><br><svg onclick=\'(()=>{const elms=[this.getElementById("play"),this.getElementById("stop")];if(playing){try{song1.pause()}catch(e){};try{song2.pause()}catch(e){};elms[0].style.opacity=1;elms[1].style.opacity=0;playing = false}else{try{song1.play()}catch(e){};try{song2.play()}catch(e){};elms[0].style.opacity=0;elms[1].style.opacity=1;playing = true}})()\' style="cursor: pointer;position: absolute;top: 260px;left: 360px;z-index: 100;" width="32px" height="32px" viewBox="0 0 32 32"><circle cx="16" cy="16" r="16" fill="#f0f0f0" /><polygon id="stop" style="opacity:0;" points="10,10 22,10, 22,22 10,22" fill="" /><polygon id="play" points="10,7 10,25 25,15" fill="" /></svg><audio src="" id="song1" volume="0.1" style="display: none;" loop="true"></audio><audio src="" id="song2" volume="0.1" style="display: none;" loop="true"></audio><script>const songs = [song1, song2];const tracks = location.hash.slice(1).split("#");const betterQuality = ["", "https://cloudflare-ipfs.com/ipfs/QmWmmmrQB3iFXHPNStyL7GvgZcifGz3JrzcFbSLQzyevjn"];for(let i =0;i<songs.length;i++) songs[i].src = (navigator.onLine && betterQuality[i]) || tracks[i];</script>';
         return
             string(
                 abi.encodePacked(
                     'data:application/json;base64,',
                     Base64.encode(
-                        abi.encodePacked(
+                        abi.encodePacked(                            
                             '{"name": "RickRolled #1", "description": "Never Gonna Give You Up is the debut single recorded by English singer and songwriter Rick Astley, released on 27 July 1987.", "image": "https://cloudflare-ipfs.com/ipfs/QmdmPHWQBzV24GvbwCszm2AnWetBENeBP2UStuETsyAp1C", "animation_url": "',
                             'data:text/html;base64,',
                             Base64.encode(bytes(html)),
@@ -35,8 +35,8 @@ contract Token is IToken, ERC721, Ownable {
                             '#',
                             getAudioAssetBase64(0),
                             '", "audio": "',
-                            getAudioAssetBase64(0),
-                            '", "external_url": "https://en.wikipedia.org/wiki/Rick_Astley", "attributes": [{"trait_type": "On-chain", "value": "Absolutely"}, {"trait_type": "Artist", "value": "Rick Astley"}, {"trait_type": "Rickrolled", "value": "You"}, {"trait_type": "Favorite Fruit", "value": "Banana"}, {"trait_type": "Favorite Blockchain", "value": "Ethereum"}], "composer": "Stock Aitken Waterman"}'
+                            getAudioAssetBase64(0),                            
+                            '", "external_url": "https://en.wikipedia.org/wiki/Rick_Astley", "attributes": [{"trait_type": "Chord Progression", "value": "2"}, {"trait_type": "First Melody", "value": "2"}, {"trait_type": "Second Melody", "value": "1"}, {"trait_type": "Third Melody", "value": "3"}, {"trait_type": "Drums", "value": "3"}], "composer": "Shaw Avery @ShawAverySongs"}'
                         )
                     )
                 )
